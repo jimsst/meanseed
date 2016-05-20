7ae27b776173975e0dc5df2058abf8370ba143ea
Jim Song <jim.song@sstid.com>
2016-05-20 05:21:18 -0400

    using phantomjs for selenium tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,6 +9,12 @@ if (process.env.TRAVIS) {
   config.capabilities = {
     browserName: 'firefox'
   };
+} else {
+  config.capabilities = {
+    browserName: 'chrome'
+  };
 }
-
+config.capabilities = {
+  browserName: 'phantomjs'
+};
 exports.config = config;
